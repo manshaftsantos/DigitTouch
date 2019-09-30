189b09aa852c4252355a19be3741753ae35fe054
Ogorgouf <mathuuser@gmail.com>
2019-09-30 18:13:09 +0100

    Ajout de footer

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<div class="main">
+  <!--==============================content================================-->
+<!--==============================footer=================================-->
+<hr>
+    <footer class ="footer">
+      <p class="paragraphe_footer"><strong class="digit_footer">DigitalTouch</strong> <span>© 2019</span></p>
+      Tous droits reservé <br>
+        
+      <div class="logo_footer">
+        <span class="twitter_footer"> <a href=""><img src="tt.png" alt="" width="40px" height="40px"></a></span>
+        <span class="facebook_footer"> <a href=""><img src="fb.png" alt="" width="32px" height="32px"></a></span>
+        <span class="mail_footer"> <a href=""><img src="cgon.png" alt="" width="32px" height="32px"></a></span>
+      </div>
+  </footer>	
+</div>  
