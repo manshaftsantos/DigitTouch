3538dc46a954f2787ac5af28bd11f7c8720d9231
Ogorgouf <mathuuser@gmail.com>
2019-10-01 13:23:05 +0100

    Ajout

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,11 @@
 <footer>
     <div>
-        <p class="digit_footer">digittouch ff &copy;2019.</p>
+        <p class="digit_footer">DigitalTouch &copy;2019.</p>
+        <p>Tous les droits reservés.</p>
+        <div class="logo">
+            <a href="twitter.com" title="Accéder à notre page twitter"><img class="logo_twitter" src="img/twitter.png" alt="" width="30px" height="30px"></a>
+            <a href="gmail.com" title="Accéder à notre page gmail"><img class="logo_gmail" src="img/gmail.png" alt="" width="30px" height="30px"></a>
+            <a href="facebook.com" title="Accéder à notre page facebook"><img class="logo_facebook" src="img/facebook.png" alt="" width="30px" height="30px"></a>
+        </div>
     </div>
 </footer>
