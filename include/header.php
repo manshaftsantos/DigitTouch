2f48850019c91b63f78438ff52b065c973f4158f
yarlOverdose <tapamow@gmail.com>
2019-10-01 14:27:57 +0100

    header php

--- include/header.php
+++ include/header.php
@@ -3,10 +3,10 @@
 
 <nav class="nav_header">
     <ul class="ul_header">
-        <li class="li_header">Acceuil</li>
-        <li class="li_header">Forum</li>
-        <li class="li_header">Contact</li>
-        <li class="li_header prop">A propos</li>
+        <li class="li_header <?php if($current == "acceuil"){ echo "current";} ?> ">Acceuil</li>
+        <li class="li_header <?php if($current == "forum"){ echo "current";} ?>">Forum</li>
+        <li class="li_header <?php if($current == "contact"){ echo "current";} ?>">Contact</li>
+        <li class="li_header prop <?php if($current == "apropos"){ echo "current";} ?>">A propos</li>
     </ul>
 </nav>
 
