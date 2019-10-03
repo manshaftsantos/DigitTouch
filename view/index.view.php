a28b8bb52bfa940242598281eb1ace0d4f83ecd5
Ogorgouf <mathuuser@gmail.com>
2019-10-03 15:41:37 +0100

    pureté 97.9

--- view/index.view.php
+++ view/index.view.php
@@ -45,16 +45,22 @@
             <p class="slogan">Nos applications mobiles sont performantes, développées pour Android et iOS 
             et offrent une expérience utilisateur optimale.</p>
             <button class="btn_savoir">En savoir plus </button>
-        </div>
-        
-        
-    </div>
-    <div class="section_one">
+    </div>    
+</div>
 
+    <div class="section_one">
+        <div class="texte_presentation">
+            <h1 class="texte_noir">Création de logos, flyers etc... </h1>
+            <p class="slogan">Blog, E-commerce, site vitrine, espace administrateur... Notre équipe a l'expérience nécessaire pour développer tout type de projet web.</p>
+            <button class="btn_savoir">En savoir plus </button>
+        </div>
+        <div class="img_presentation">
+            <img src="img/logo_flyers.png" alt="pc" >
+        </div>
     </div>
 
 </section>
-<!---------Footer------------------------------------------->
+
 <?php include("include/footer.php")?>
 </body>
 </html>
