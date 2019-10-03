74d3b5ad2e1ed0d7625b633a72c74be16b74c199
manshaftsantos <manshaftsantos@gmail.com>
2019-10-03 14:00:40 +0200

    h

---
+++ view/forum.view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <?php $current = "forum" ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include("include/link.php") ?>
+    <title>Forum</title>
+</head>
+<body>
+   
+<?php include("include/header.php")?>
+<div class="barre_info"></div>
+
+    <section class="section"> 
+        <div class="zone_message">
+           
+        </div> 
+    </section>
+
+
+
+<?php include("include/footer.php")?>
+</body>
+</html>
