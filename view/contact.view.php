177ae9f8f53ba908866781bf928742e6d19a906b
Ogorgouf <mathuuser@gmail.com>
2019-10-07 14:36:58 +0100

    Pit

--- view/contact.view.php
+++ view/contact.view.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <?php $current = "acceuil" ?>
+    <?php $current = "contact" ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include("include/link.php") ?>
