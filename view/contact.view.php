7cd0d93704b6959e93a28f8badbae537d4eb9188
YarlOverdose <tapamow@gmail.com>
2019-10-07 14:31:55 +0100

    debut

---
+++ view/contact.view.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <?php $current = "acceuil" ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include("include/link.php") ?>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Acceuil</title>
+</head>
+<body>
+<!---------Header------------------------------------------->
+<?php include("include/header.php")?>
+
+    
+<!---------Footer------------------------------------------->
+<?php include("include/footer.php")?>
+</body>
+</html>
