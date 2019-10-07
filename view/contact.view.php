2576c5adc7f54d74168b15df62640ace8c90d819
Ogorgouf <mathuuser@gmail.com>
2019-10-07 16:27:59 +0100

    f

--- view/contact.view.php
+++ view/contact.view.php
@@ -10,7 +10,29 @@
 <body>
 <!---------Header------------------------------------------->
 <?php include("include/header.php")?>
-
+<section class="scontact">
+    <div align="center" class="mcontact">
+        <h1>Nous contacter</h1>
+        <hr id="hr1">
+        <hr id="hr2">
+    </div>
+    <div class="tcontact" align="center">
+        <p>Vous souhaitez discuter de votre projet ? Obtenir un devis ? 
+        Vous avez <br> une quelconque question ? N'hésitez pas à nous contacter.</p>
+    </div>
+    <form  method="POST" action="">
+        
+        <div class="cform" >    
+                <div><label class="fv" for="lnom">Votre nom*</label><br><br><input type="text" name="lnom"></div>
+                <div><label class="fv" for="lmail">Votre Email*</label><br><br><input type="email" name="lmail"></div>
+                <div><label class="fv" for="ltel">Téléphone*</label><br><br><input type="tel" name="ltel"></div>
+                <div><label class="fv" for="lmessage">Message*</label><br><br><textarea name="lmessage" id="" cols="138" rows="10"></textarea></div>
+                
+                
+        </div>
+              <br>  <input class="btn_savoir lsub" type="submit" value="Envoyer">
+    </form>
+</section>
 
 <!---------Footer------------------------------------------->
 <?php include("include/footer.php")?>
