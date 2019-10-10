2f2aa1ca34c31e58a0d802079cafc15585f5dc54
Ogorgouf <55984058+Ogorgouf@users.noreply.github.com>
2019-10-10 12:30:08 +0100

    Firewall

--- view/esp_dev_mobile.view.php
+++ view/esp_dev_mobile.view.php
@@ -11,7 +11,49 @@
 <body>
 <!---------Header------------------------------------------->
 <?php include("include/header.php")?>
+<section class="scontact">
+    <div align="center" class="mcontact">
+        <h2>Notre Savoir-Faire</h2>
+        <hr id="hr1">
+        
+    </div>
+    <div class="tcontact" align="center">
+        <p class="slogan">Nos prestations sont sur mesure et uniques. <br> 
+            Chaque réalisation embarque notre expertise dans les domaines suivants.</p>
+    </div>
 
+    <div class="sdeveloppement">
+        <div class="dev">
+                <img  class="iphone" src="img/iphone.png" alt="">
+                <h2>Création d'application pour ios</h2>
+                <p class="slogan"> Iphone aujourd'hui a une grande communauté d'utilisateur
+                alors pensez à créer votre application ios à but lucratif .</p>
+        </div>
+        <div class="dev andro">
+                <img class="android" src="img/android.png" alt="">
+                <h2>Création d'application pour android</h2>
+                <p class="slogan">Devenu le système mobile du 21<sup>e</sup>siècle, c'est à dire
+                qu'il est très répandu sur le globe alors confiez nous votre idée et nous la
+                transformons en application android révolutionnaire.</p> 
+        </div>
+        <div class="dev andro">
+                <img class="android" src="img/android.png" alt="">
+                <h2>Création d'application pour android</h2>
+                <p class="slogan">Devenu le système mobile du 21<sup>e</sup>siècle, c'est à dire
+                qu'il est très répandu sur le globe alors confiez nous votre idée et nous la
+                transformons en application android révolutionnaire.</p> 
+        </div>
+    </div>
+    
+    <div align="center">
+        <h2>Besoin</h2>
+        <hr id="hr1">
+        <hr id="hr2">
+        <p class="slogan">Besoin d'un site web? notre equipe vous offre <br> un excellent design,  une tres bonne visibilité et une bonne structure </p>
+        <a href="contact.php"><button class="btn_savoir"> Contactez nous </button> </a>
+    </div>
+    
+</section>
 <!---------Footer------------------------------------------->
 <?php include("include/footer.php")?>
 </body>
