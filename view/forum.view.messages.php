a6e77787e3db5d58355cb776688e07ed37eb004b
YarlOverdose <tapamow@gmail.com>
2019-10-15 14:11:56 +0100

    java

--- view/forum.view.messages.php
+++ view/forum.view.messages.php
@@ -10,23 +10,46 @@
 </head>
 <body>
 <?php include("include/header.php") ?>
-
+<section id="con" align='center'>
+<img id="btn_close1" src="img/close.png" width="70" title="Fermer" alt="">
+   <hr>
+    <h1>CONNEXION</h1>
+   <form action="" method="post">
+      <input required type="text" name="cpseudo" placeholder="Pseudo"> <br> <br>
+      <input required  type="password" name="cmdp" placeholder="Mot de passe"> <br> <br>
+      <input class="btn_savoir" type="submit" value="connexion">
+   </form>
+</section>
+<section id="nco" align='center'>
+   <img id="btn_close" src="img/close.png" width="70" title="Fermer" alt=""> 
+   <hr>
+    <h1>Nouveau Compte</h1>
+   <form id="io" action="" method="post">
+      <input class="ip" required type="text" name="pseudo" placeholder="Pseudo"> <br> <br>
+      <input class="ip" type="password" name="mdp" placeholder="Mot de passe"> <br> <br>
+      <input class="ip" type="password" name="mdp2" placeholder="Verifier mot de passe"> <br> <br>
+      <input class="ip" type="email" name="email" placeholder="Email"> <br> <br>
+      <input  class="btn_savoir ip" type="submit" value="ENREGISTRER">
+   </form>
+</section>
 
   
 
 <section class="zone_message">  
          <nav class="nav_forum">
                <ul class="ul_forum">
-                  <a  class="a_forum active" href="forum.messages.php"><li class="li_forum active">messages recents</li></a>
-                  <a href="forum.php"class="a_forum" > <li class="li_forum">posez votre problème</li></a>
-                  <a href="sujet.php"class="a_forum " ><li class="li_forum">sujets</li></a>
+                  <a  class="a_forum active" href="forum.messages.php"><li class="li_forum active">Méssages récents</li></a>
+                  <a href="forum.php"class="a_forum" > <li class="li_forum">Posez votre problème</li></a>
+                  <a href="sujet.php"class="a_forum " ><li class="li_forum">Sujets</li></a>
+                  <button id="co" >Connexion</button>
+                  <button id="nc">Nouveau Compte</button>
                </ul>
          </nav>
       <a href="reponses.php">      
          <div class="div_msg2">
             <div class="pseudo_msg">
                <div class="user_pseudo" >
-                  <div class="psdo">pseudo</div>
+                  <div class="psdo">Pseudo</div>
                   <div class="pp" >pp</div>
                </div>
                <div class="user_msg" >
@@ -73,7 +96,7 @@
 
 
 
-
+<script src="js/main.js"></script>
 <?php include("include/footer.php") ?>
 </body>
 </html>
